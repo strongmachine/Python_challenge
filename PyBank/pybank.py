a11aea3b30e52612c99bb9e5e980d97bae3d8b44
adrianlongoria <strongmachine@icloud.com>
2021-08-07 20:28:48 -0500

    Budget analysis

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -1,8 +1,62 @@
 import os
 import csv
 
-total_votes = 0 
-with open("Resources/budget_data.csv", 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+row = []
+with open("Resources/budget_data.csv", 'r') as file_handler:
+    print(file_handler)
+    csv_reader = csv.reader(file_handler, delimiter=",")
+    head = next(csv_reader)
+    print(head)
+    first_row =  next(csv_reader)
+    print(first_row)
+    change = []
+    sum = int(first_row[1])
+    row_count = 1
+    prev_change = int(first_row[1])
+    net_change = 0
+    max_change = 0
+    min_change = 0
+    max_change_month = ""
+    min_change_month = ""
     for row in csv_reader:
-        
+        row_count += 1
+        row[1] = int(row[1])
+        sum += row[1] 
+        net_change = row[1]-prev_change
+        change.append(net_change)
+        prev_change = row[1]
+        prev_net = int(row[1])
+        net_change = int(row[1]) - prev_net
+       
+        max_change_month = max_change_month + row[0]
+        if (net_change > max_change):
+            max_change[0] = row["max_change_month"]
+            max_change[1] = prev_change
+        min_change_month = max_change_month + row[0]   
+        if (net_change < min_change):
+         min_change[0] = row["min_change_month"]
+         min_change[1] = prev_change
+
+#net_monthly_avg = sum(net_change_list) / len(net_change_list)
+            
+       
+    
+chng = 0
+for x in change:
+    chng += x
+# print(chng, len(change))
+ret = f"""
+Financial Analysis
+  ----------------------------
+  Total Months: {row_count}
+  Total: ${sum}
+  Average  Change: ${(chng/len(change))}
+  Greatest Increase in Profits: {max_change_month[0]} (${max_change})
+  Greatest Decrease in Profits: {min_change_month[0]} (${min_change})
+"""
+print(ret)
+
+       
+   with open("budget_data.txt", 'w') as txt_file:
+    txt_file.write(ret)
+
